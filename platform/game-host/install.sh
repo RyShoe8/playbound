ddb50c62e3a10f49d9f43960a6948a975616a70a
RyShoe8 <30275036+RyShoe8@users.noreply.github.com>
2026-09-25 03:28:05 -0500

    Include A2S query module in VPS agent install

--- platform/game-host/install.sh
+++ platform/game-host/install.sh
@@ -215,7 +215,7 @@ cp -f "$AGENT_SRC/index.js" "$AGENT_SRC/agentRoutes.js" "$AGENT_SRC/recipes.js" 
   "$AGENT_SRC/etLegacyInstall.js" "$AGENT_SRC/metrics.js" "$AGENT_SRC/spawnTests.js" \
   "$AGENT_SRC/gameVersions.js" "$AGENT_SRC/rcon.js" "$AGENT_SRC/roomRestart.js" \
   "$AGENT_SRC/startLock.js" "$AGENT_SRC/downloadStream.js" \
-  "$AGENT_SRC/managedRegistry.js" "$AGENT_SRC/processMetrics.js" \
+  "$AGENT_SRC/managedRegistry.js" "$AGENT_SRC/processMetrics.js" "$AGENT_SRC/a2sQuery.js" \
   "$AGENT_SRC/communityHostingTick.js" \
   "$AGENT_SRC/package.json" "$AGENT_DIR/"
 mkdir -p "$AGENT_DIR/assets"
